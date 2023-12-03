8f8127639791979316779926109921517b327373
Caroalv <alvaradocarolinad2002@gmail.com>
2023-12-03 11:09:57 -0600

    testeos

--- tests/Unit/LogicTest.php
+++ tests/Unit/LogicTest.php
@@ -23,7 +23,65 @@ class LogicTest extends TestCase //esta define una clase de prueba que hereda de
     //PRUEBA PARA SUMAR
 
     public function testSumar(){
-        $result = $this->logic->add(2,3); //llama al metodo add de LOgic con los argumentos 2 y 3
+        $result = $this->logic->add(2,3); //llama al metodo add de Logic con los argumentos 2 y 3
         $this->assertEquals(5, $result); //verifica que el resultado sea igual a 5
+    }
+
+    public function testRestar(){
+        $result = $this->logic->restar(5,2); //llama al metodo restar de Logic con los argumentos 5 y 2
+        $this->assertEquals(3, $result); //verifica que el resultado sea igual a 3
+    }
+
+    public function testMul(){
+        $result = $this->logic->mul(2,3); //llama al metodo add de Logic con los argumentos 2 y 3
+        $this->assertEquals(6, $result); //verifica que el resultado sea igual a 6
+    }
+    public function testDiv(){
+        $result = $this->logic->div(6,2); //llama al metodo add de Logic con los argumentos 6 y 2
+        $this->assertEquals(3, $result); //verifica que el resultado sea igual a 3
+    }
+
+    public function testverdadero()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testfalso()
+    {
+        $this->assertFalse(false);
+    }
+
+    public function testigualdad()
+    {
+    $result = $this->logic->igualdad(2,2); //llama al metodo add de Logic con los argumentos 6 y 2
+    $this->assertEquals(3, 3, $result); //verifica que el resultado sea igual a 
+    }
+
+    public function testdiferente()
+    {
+    $this->assertNotEquals(1, 3); //verifica que el resultado sea igual a 
+    }
+
+    public function testArrayCuente()
+    {
+        $array = [1,2,3];
+        $this->assertCount(3,$array);
+    }
+
+    public function testnulo()
+    {
+    $valor = null;    
+    $this->assertNull($valor); //verifica que el resultado sea igual a 
+    }
+
+    public function testnonulo()
+    {
+    $valor = 'hola';    
+    $this->assertNotNull($valor); //verifica que el resultado sea igual a 
+    }
+    public function testmayormenor()
+    {
+    $valor = '5';    
+    $this->assertGreaterThanOrEqual(5, $valor); //verifica que el resultado sea igual a 
     }
 }
--- tests/Unit/logic/logic.php
+++ tests/Unit/logic/logic.php
@@ -12,5 +12,59 @@
             return $a + $b;
 
         }
+
+        public function restar($a, $b){
+            return $a - $b;
+
+        }
+
+        public function mul($a, $b){
+            return $a * $b;
+
+        }
+
+        public function div($a, $b){
+            return $a/$b;
+
+        }
+
+        
+        public function verdadero($valor)
+        {
+        return $valor === true;
+        }
+        public function falso($valor)
+        {
+        return $valor === false;
+        }
+
+        public function igualdad($a, $b){
+        return $a === $b;
+        }
+
+        public function diferente($a, $b){
+            return $a !== $b;
+    
+            }
+
+
+        public function checkArrayCount($count, $array)
+        {
+            return count($array)=== $count;
+        }
+        public function nulo($valor)
+        {
+            return is_null($valor);
+        } 
+
+        public function nonulo($valor)
+        {
+            return !is_null($valor);
+        } 
+
+        public function mayormenor($valor, $comparacion)
+        {
+            return $valor >=$comparacion;
+        } 
     }
 ?>
